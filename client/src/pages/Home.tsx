0ce57cad1c2478412eb671194c98ad5a203e3c93
ChienTQ2003 <anhchienbs.1@gmail.com>
2025-05-22 23:29:44 +0700

    commit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -391,60 +391,7 @@ const Home: React.FC = () => {
             </div>
           </div>
         </section>
-        <section className="banner-block">
-          <div className="tw-relative tw-w-full banner-block__wrapper">
-            <a
-              href="#"
-              className="tw-block tw-w-full"
-              ga-tracking-value="bannersection-vitri1"
-              ga-tracking-label="SẢN PHẨM MẶC HẰNG NGÀY"
-            >
-              <div className="tw-w-full tw-h-full">
-                <img
-                  loading="lazy"
-                  src="images/Casual_-_Desktop.jpg"
-                  alt="CASUALWEAR COLLECTION"
-                  style={{ width: "100%", aspectRatio: 3, objectFit: "cover" }}
-                />
-              </div>
-            </a>
-            <div className="banner-block__content black container">
-              <h2
-                className="banner-block__heading "
-                style={{ textTransform: "uppercase" }}
-              >
-                CASUALWEAR COLLECTION
-              </h2>
-              <p className="banner-block__descriptions ">
-                Nhập COOLNEW Giảm 30k đơn đầu tiên từ 199k
-              </p>
-              <div className="banner-block__button">
-                <a
-                  className="tw-inline-flex tw-items-center tw-gap-1 md:tw-gap-2 tw-font-criteria md:tw-text-base tw-text-sm tw-font-normal tw-px-3 md:tw-px-12 tw-pt-2 tw-pb-[5px] md:tw-py-4 tw-rounded-full tw-uppercase tw-bg-cm-neutral-00 tw-text-cm-neutral-900 hover:tw-bg-cm-neutral-100"
-                  href=""
-                  ga-tracking-value="bannersection-vitri1"
-                  ga-tracking-label="SẢN PHẨM MẶC HẰNG NGÀY"
-                >
-                  MUA NGAY
-                  <span className="tw-mb-1">
-                    <svg
-                      width={20}
-                      height={12}
-                      viewBox="0 0 20 12"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M19.5303 6.53033C19.8232 6.23744 19.8232 5.76256 19.5303 5.46967L14.7574 0.696698C14.4645 0.403804 13.9896 0.403805 13.6967 0.696698C13.4038 0.989591 13.4038 1.46446 13.6967 1.75736L17.9393 6L13.6967 10.2426C13.4038 10.5355 13.4038 11.0104 13.6967 11.3033C13.9896 11.5962 14.4645 11.5962 14.7574 11.3033L19.5303 6.53033ZM6.55671e-08 6.75L19 6.75L19 5.25L-6.55671e-08 5.25L6.55671e-08 6.75Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
+        
         <section className="homepage-products">
           <div className="container">
             <div className="homepage-products__heading mac-hang-ngay-trang-chu">
@@ -549,60 +496,6 @@ const Home: React.FC = () => {
               >
                 Xem thêm
               </a>
-            </div>
-          </div>
-        </section>
-        <section className="banner-block">
-          <div className="tw-relative tw-w-full banner-block__wrapper">
-            <a
-              href=""
-              className="tw-block tw-w-full"
-              ga-tracking-value="bannersection-vitri2"
-              ga-tracking-label="SẢN PHẨM CHẠY BỘ"
-            >
-              <div className="tw-w-full tw-h-full">
-                <img
-                  loading="lazy"
-                  src="images/Running_-_Desktop.jpg"
-                  alt="RUNNING COLLECTION"
-                  style={{ width: "100%", aspectRatio: 3, objectFit: "cover" }}
-                />
-              </div>
-            </a>
-            <div className="banner-block__content black container">
-              <h2
-                className="banner-block__heading "
-                style={{ textTransform: "uppercase" }}
-              >
-                RUNNING COLLECTION
-              </h2>
-              <p className="banner-block__descriptions ">
-                Nhập COOLNEW Giảm 30k đơn đầu tiên từ 199k
-              </p>
-              <div className="banner-block__button">
-                <a
-                  className="tw-inline-flex tw-items-center tw-gap-1 md:tw-gap-2 tw-font-criteria md:tw-text-base tw-text-sm tw-font-normal tw-px-3 md:tw-px-12 tw-pt-2 tw-pb-[5px] md:tw-py-4 tw-rounded-full tw-uppercase tw-bg-cm-neutral-00 tw-text-cm-neutral-900 hover:tw-bg-cm-neutral-100"
-                  href=""
-                  ga-tracking-value="bannersection-vitri2"
-                  ga-tracking-label="SẢN PHẨM CHẠY BỘ"
-                >
-                  MUA NGAY
-                  <span className="tw-mb-1">
-                    <svg
-                      width={20}
-                      height={12}
-                      viewBox="0 0 20 12"
-                      fill="currentColor"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M19.5303 6.53033C19.8232 6.23744 19.8232 5.76256 19.5303 5.46967L14.7574 0.696698C14.4645 0.403804 13.9896 0.403805 13.6967 0.696698C13.4038 0.989591 13.4038 1.46446 13.6967 1.75736L17.9393 6L13.6967 10.2426C13.4038 10.5355 13.4038 11.0104 13.6967 11.3033C13.9896 11.5962 14.4645 11.5962 14.7574 11.3033L19.5303 6.53033ZM6.55671e-08 6.75L19 6.75L19 5.25L-6.55671e-08 5.25L6.55671e-08 6.75Z"
-                        fill="currentColor"
-                      />
-                    </svg>
-                  </span>
-                </a>
-              </div>
             </div>
           </div>
         </section>
